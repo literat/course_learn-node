0cf980d4bc470b034e800b929ac42cd4f9d9c5a6
literat <tomas@litera.me>
2020-03-08 22:02:03 +0100

    Core Concept - Middleware and Error Handling

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,10 @@
+exports.myMiddleware = (req, res, next) => {
+  req.name = 'Wes';
+  req.cookie('name', 'Wes is cool', { maxAge: 9000000})
+  next();
+}
+
 exports.homePage = (req, res) => {
+  console.log(req.name)
   res.render('index');
 }
