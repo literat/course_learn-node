e43ed5d6d6bb85420704d2425b78735662a3486b
literat <tomas@litera.me>
2020-09-28 22:10:05 +0200

    Creating a User Account Edit Screen

--- routes/index.js
+++ routes/index.js
@@ -43,4 +43,7 @@ router.post('/register',
 
 router.get('/logout', authController.logout);
 
+router.get('/account', authController.isLoggedIn, userController.account);
+router.post('/account', catchErrors(userController.updateAccount));
+
 module.exports = router;
