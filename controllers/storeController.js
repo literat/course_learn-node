7c9c56ba1994b69b90284e5b2876278bb56518ba
literat <tomas@litera.me>
2020-10-05 15:17:22 +0200

    JSON endpoints and creating MongoDB Indexes

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -100,3 +100,22 @@ exports.getStoresByTag = async (req, res) => {
 
   res.render('tag', {tags, title: 'Tags', tag, stores });
 }
+
+exports.searchStores = async (req, res) => {
+  const stores = await Store
+  // first find stores that match
+  .find({
+    $text: {
+      $search: req.query.q,
+    }
+  }, {
+    score: { $meta: 'textScore' }
+  })
+  // the sort them
+  .sort({
+    score: { $meta: 'textScore' }
+  });
+  // limit to only 5 result
+  .limit(5);
+  res.json(stores);
+}
--- routes/index.js
+++ routes/index.js
@@ -52,4 +52,9 @@ router.post('/account/reset/:token',
   catchErrors(authController.update)
 );
 
+/*
+  API
+*/
+router.get('/api/search', catchErrors(storeController.searchStores));
+
 module.exports = router;
