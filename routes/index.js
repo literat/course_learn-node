89d1332563af8c156c5d20f3de11b5fa5caa371d
literat <tomas@litera.me>
2020-10-20 10:05:15 +0200

    Advanced Aggregation

--- routes/index.js
+++ routes/index.js
@@ -55,6 +55,7 @@ router.post('/account/reset/:token',
 router.get('/map', storeController.mapPage);
 router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts));
 router.post('/reviews/:id', authController.isLoggedIn, catchErrors(reviewController.addReview));
+router.get('/top', catchErrors(storeController.getTopStores));
 
 /*
   API
